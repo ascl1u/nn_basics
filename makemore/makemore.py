14b2071541673cca7848afce8f80744b284c0b89
ascl1u <a.liu27568@gmail.com>
2025-10-27 11:49:38 -0700

    makemore mlp implementation

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -7,6 +7,8 @@ from dataclasses import dataclass
 class ModelConfig:
     block_size = None # length of input sequences
     vocab_size = None # input in range [0 ... vocab_size - 1]
+    n_embd: int = 32
+    n_embd2: int = 32
 
 class Bigram(nn.Module):
     
@@ -23,4 +25,30 @@ class Bigram(nn.Module):
         loss = None
         if targets is not None:
             loss = F.cross_entropy(logits, targets)
+        return logits, loss
+    
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.block_size = config.block_size
+        self.vocab_size = config.vocab_size
+        self.wte = nn.Embedding(self.vocab_size, config.n_embd)
+        self.mlp = nn.Sequential(
+            nn.Linear(self.block_size * config.n_embd, config.n_embd2),
+            nn.Tanh(),
+            nn.Linear(config.n_embd2, self.vocab_size)
+        )
+
+    def get_block_size(self):
+        return self.block_size
+    
+    def forward(self, idx, targets=None):
+        # idx shape: (batch_size, block_size)
+        emb = self.wte(idx) # (batch_size, block_size, n_embd)
+        x = emb.view(emb.size(0), -1) # flatten to (batch_size, block_size * n_embd)
+        logits = self.mlp(x) # (batch_size, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits, targets.view(-1)) # flatten to (batch_size, )
         return logits, loss
